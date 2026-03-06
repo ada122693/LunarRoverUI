38c6065983da65171eb066eeda6424bd9c3218e8
Amanda Parada <aparada@lechuza.cafe>
2026-03-05 20:28:13 -0500

    Remove build folder from git repository

--- build/CMakeFiles/LunarRoverUI.dir/LunarRoverUI_autogen/EWIEGA46WW/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/LunarRoverUI.dir/LunarRoverUI_autogen/EWIEGA46WW/qrc_resources.cpp.o: \
- /mnt/datadrive/tareas/spring2026/csproject/LunarRoverUI/build/LunarRoverUI_autogen/EWIEGA46WW/qrc_resources.cpp \
- /usr/include/stdc-predef.h
